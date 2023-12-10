b5d5dcaea729e0b1f000197b12536064e4425066
KhoaaTran <88638031+KhoaaTran@users.noreply.github.com>
2023-12-10 08:00:23 +0700

    Add Filter

--- assets/js/scrips.js
+++ assets/js/scrips.js
@@ -114,8 +114,7 @@ function handleActiveMenu() {
       if (!items.length) return
 
       removeActive(menu)
-      if (window.innerWidth > 991)
-      items[0].classList.add(activeClass)
+      if (window.innerWidth > 991) items[0].classList.add(activeClass)
 
       Array.from(items).forEach((item) => {
         item.onmouseenter = () => {
@@ -156,7 +155,8 @@ function initJsToggle() {
     if (!target) {
       document.body.innerText = `Cần thêm toggle-target cho: ${button.outerHTML}`
     }
-    button.onclick = () => {
+    button.onclick = (e) => {
+      e.preventDefault()
       if (!$(target)) {
         return (document.body.innerText = `Không tìm thấy phần tử "${target}"`)
       }
@@ -167,17 +167,26 @@ function initJsToggle() {
         $(target).classList.toggle('show', isHidden)
       })
     }
+
+    document.onclick = function (e) {
+      if (!e.target.closest(target)) {
+        const isHidden = $(target).classList.contains('hide')
+        if (!isHidden) {
+          button.click()
+        }
+      }
+    }
   })
 }
 
-window.addEventListener("template-loaded", () => {
-  const links = $$(".js-dropdown-list > li > a");
+window.addEventListener('template-loaded', () => {
+  const links = $$('.js-dropdown-list > li > a')
 
   links.forEach((link) => {
-      link.onclick = () => {
-          if (window.innerWidth > 991) return;
-          const item = link.closest("li");
-          item.classList.toggle("navbar__item--active");
-      };
-  });
-});
+    link.onclick = () => {
+      if (window.innerWidth > 991) return
+      const item = link.closest('li')
+      item.classList.toggle('navbar__item--active')
+    }
+  })
+})
